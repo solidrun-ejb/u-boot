fd6f533db97a1ed2554f40ff889e2178f03890b2
Jon Nettleton <jon@solid-run.com>
2019-01-04 08:54:22 +0100

    imx: imx8mq_hb: update board usb initialization
    This updates our initialization code to work with the DWC3
    generic framework.  It makes use of helper functions in previous
    commits.
    
    Signed-off-by: Jon Nettleton <jon@solid-run.com>

--- board/solidrun/imx8mq_hb/imx8mq_hb.c
+++ board/solidrun/imx8mq_hb/imx8mq_hb.c
@@ -24,6 +24,7 @@
 #include <usb.h>
 #include <dwc3-uboot.h>
 #include <linux/usb/dwc3.h>
+#include <dm.h>
 #include "../../freescale/common/pfuze.h"
 #include "../../freescale/common/tcpc.h"
 
@@ -134,59 +135,6 @@ int board_phy_config(struct phy_device *phydev)
 }
 #endif
 
-#if defined(CONFIG_USB_DWC3) || defined(CONFIG_USB_XHCI_IMX8M)
-
-#define USB_PHY_CTRL0                   0xF0040
-#define USB_PHY_CTRL0_REF_SSP_EN        BIT(2)
-
-#define USB_PHY_CTRL1                   0xF0044
-#define USB_PHY_CTRL1_RESET             BIT(0)
-#define USB_PHY_CTRL1_COMMONONN         BIT(1)
-#define USB_PHY_CTRL1_ATERESET          BIT(3)
-#define USB_PHY_CTRL1_VDATSRCENB0       BIT(19)
-#define USB_PHY_CTRL1_VDATDETENB0       BIT(20)
-
-#define USB_PHY_CTRL2                   0xF0048
-#define USB_PHY_CTRL2_TXENABLEN0        BIT(8)
-
-static struct dwc3_device dwc3_device_data = {
-        .maximum_speed = USB_SPEED_SUPER,
-        .base = USB1_BASE_ADDR,
-        .dr_mode = USB_DR_MODE_PERIPHERAL,
-        .index = 0,
-        .power_down_scale = 2,
-};
-
-int usb_gadget_handle_interrupts(void)
-{
-        dwc3_uboot_handle_interrupt(0);
-        return 0;
-}
-
-static void dwc3_nxp_usb_phy_init(struct dwc3_device *dwc3)
-{
-        u32 RegData;
-
-        RegData = readl(dwc3->base + USB_PHY_CTRL1);
-        RegData &= ~(USB_PHY_CTRL1_VDATSRCENB0 | USB_PHY_CTRL1_VDATDETENB0 |
-                        USB_PHY_CTRL1_COMMONONN);
-        RegData |= USB_PHY_CTRL1_RESET | USB_PHY_CTRL1_ATERESET;
-        writel(RegData, dwc3->base + USB_PHY_CTRL1);
-
-        RegData = readl(dwc3->base + USB_PHY_CTRL0);
-        RegData |= USB_PHY_CTRL0_REF_SSP_EN;
-        writel(RegData, dwc3->base + USB_PHY_CTRL0);
-
-        RegData = readl(dwc3->base + USB_PHY_CTRL2);
-        RegData |= USB_PHY_CTRL2_TXENABLEN0;
-        writel(RegData, dwc3->base + USB_PHY_CTRL2);
-
-        RegData = readl(dwc3->base + USB_PHY_CTRL1);
-        RegData &= ~(USB_PHY_CTRL1_RESET | USB_PHY_CTRL1_ATERESET);
-        writel(RegData, dwc3->base + USB_PHY_CTRL1);
-}
-#endif
-
 #ifdef CONFIG_USB_TCPC
 struct tcpc_port port;
 struct tcpc_port_config port_config = {
@@ -230,18 +178,38 @@ static int setup_typec(void)
 }
 #endif
 
-#if defined(CONFIG_USB_DWC3) || defined(CONFIG_USB_XHCI_IMX8M)
-int board_usb_init(int index, enum usb_init_type init)
+
+#if defined(CONFIG_USB)
+int board_usb_of_init(struct udevice *dev)
 {
-        int ret = 0;
+        int ret, index, init = 0;
+	enum usb_dr_mode dr_mode = USB_DR_MODE_HOST;
+	struct udevice *dwc3 = NULL;
+
+	index = imx8m_get_usb_index(dev);
+
+	ret = device_find_first_child(dev, &dwc3);
+        if (ret)
+                return ret;
+
+	if (dwc3) {
+		dr_mode = usb_get_dr_mode(dev_of_offset(dwc3));
+	}
+
+	if (dr_mode > USB_DR_MODE_HOST)
+		init = USB_INIT_DEVICE;
+	else
+		init = USB_INIT_HOST;
+
+	printf("board_usb_of_init index: %d, init = %d\n", index, init);
+
+	init_usb_clk(index);
         imx8m_usb_power(index, true);
 
         if (index == 0 && init == USB_INIT_DEVICE) {
 #ifdef CONFIG_USB_TCPC
                 ret = tcpc_setup_ufp_mode(&port);
 #endif
-                dwc3_nxp_usb_phy_init(&dwc3_device_data);
-                return dwc3_uboot_init(&dwc3_device_data);
         } else if (index == 0 && init == USB_INIT_HOST) {
 #ifdef CONFIG_USB_TCPC
                 ret = tcpc_setup_dfp_mode(&port);
@@ -252,12 +220,30 @@ int board_usb_init(int index, enum usb_init_type init)
         return 0;
 }
 
-int board_usb_cleanup(int index, enum usb_init_type init)
+void board_usb_of_cleanup(struct udevice *dev)
 {
-        int ret = 0;
-        if (index == 0 && init == USB_INIT_DEVICE) {
-                dwc3_uboot_exit(index);
-        } else if (index == 0 && init == USB_INIT_HOST) {
+        int ret, index, init = 0;
+	enum usb_dr_mode dr_mode = USB_DR_MODE_HOST;
+	struct udevice *dwc3 = NULL;
+
+	index = imx8m_get_usb_index(dev);
+
+	ret = device_find_first_child(dev, &dwc3);
+        if (ret)
+                return;
+
+	if (dwc3) {
+		dr_mode = usb_get_dr_mode(dev_of_offset(dwc3));
+	}
+
+	if (dr_mode > USB_DR_MODE_HOST)
+		init = USB_INIT_DEVICE;
+	else
+		init = USB_INIT_HOST;
+
+	printf("board_usb_of_init index: %d, init = %d\n", index, init);
+
+        if (index == 0 && init == USB_INIT_HOST) {
 #ifdef CONFIG_USB_TCPC
                 ret = tcpc_disable_src_vbus(&port);
 #endif
@@ -265,7 +251,7 @@ int board_usb_cleanup(int index, enum usb_init_type init)
 
         imx8m_usb_power(index, false);
 
-        return ret;
+        return;
 }
 #endif
 
@@ -293,8 +279,6 @@ int ft_board_setup(void *blob, bd_t *bd)
 	return 0;
 }
 #endif
-
-void init_usb_clk(int usbno);
 
 int board_late_init(void)
 {
