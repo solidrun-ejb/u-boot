235dcf17c94b4b6655b938775c0ca1e0bfbaa65b
Jon Nettleton <jon@solid-run.com>
2019-01-04 09:21:46 +0100

    imx: imx8mq_hb: Enable phylib support for ethernet
    This switches us over to using the device-tree to intialize
    the fec ethernet driver and associated phy.  It allows us
    to remove some of the board specific phy initialization code.
    
    Signed-off-by: Jon Nettleton <jon@solid-run.com>

--- include/configs/imx8mq_hb.h
+++ include/configs/imx8mq_hb.h
@@ -81,16 +81,10 @@
 #define CONFIG_ETHPRIME			"FEC"
 
 #define CONFIG_FEC_MXC
-#define CONFIG_FEC_XCV_TYPE		RGMII
-#define CONFIG_FEC_MXC_PHYADDR		4
 #define FEC_QUIRK_ENET_MAC
-#define GP_RGMII_PHY_RESET		IMX_GPIO_NR(1, 9)
 
 #define CONFIG_PHY_GIGE
 #define IMX_FEC_BASE			0x30BE0000
-
-#define CONFIG_PHYLIB
-#define CONFIG_PHY_ATHEROS
 #endif
 
 /* Link Definitions */
